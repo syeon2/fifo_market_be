91dbb0db18ed81a6fa5d4acb79166e586acada9c
SuYeon Kim <waterkite94@gmail.com>
2025-05-24 16:05:00 +0900

    init: project

---
+++ src/main/java/proj/syeon2/fifo_market/FifoMarketApplication.java
@@ -0,0 +1,13 @@
+package proj.syeon2.fifo_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifoMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifoMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/proj/syeon2/fifo_market/FifoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package proj.syeon2.fifo_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifoMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
