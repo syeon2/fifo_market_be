c29711d99cac562127711d93ab99dcac5cfc8be3
SuYeon Kim <waterkite94@gmail.com>
2025-05-24 17:26:28 +0900

    chore: add code style rules with Checkstyle and formatter

--- src/main/java/proj/syeon2/fifomarket/FifoMarketApplication.java
+++ src/main/java/proj/syeon2/fifomarket/FifoMarketApplication.java
@@ -1,4 +1,4 @@
-package proj.syeon2.fifo_market;
+package proj.syeon2.fifomarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FifoMarketApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FifoMarketApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FifoMarketApplication.class, args);
+	}
 
 }
